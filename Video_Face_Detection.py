076518429c5a1d655888c4ab94ccc973ace00008
Debjit Bhowmik <debjitbhowmik807@gmail.com>
2020-10-07 10:20:53 +0530

    First Commit

---
+++ Video_Face_Detection.py
@@ -0,0 +1,24 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#Read the input image
+#img = cv2.imread('Deb.jpg')
+cap = cv2.VideoCapture('Taylor.mp4')
+
+while cap.isOpened():
+	_, img = cap.read()
+
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.1 , 4)
+
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 3)
+
+	#Dispaly the output
+
+	cv2.imshow('Debjit_Eco', img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
